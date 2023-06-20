90b902a50659448d04e2f524b508aa1b2da9f98c
Leticia <leticia.negreiros@outlook.com>
2023-06-20 10:02:36 -0300

    chore: adição de comentário no documento js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ let errorMessage = document.getElementById("error-message");
 let successMessage = document.getElementById("success-message");
 
 form.addEventListener("submit", function (event) {
-  event.preventDefault();
+  event.preventDefault(); // Impede que o formulário atualize assim que o botão de submit for ativado. Se não colocarmos essa condição, as mensagens de sucesso ou erro mal aparecerão, porque a página será atualizada logo em seguida
 
   let formEValido = numB.value >= numA.value; // Adicionar a condição no escopo do event listener, ou escrevê-la como uma função fora do escopo do event listener
 
