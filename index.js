0885c5565034c777b7f386eefdb2f24e397a1c75
Leticia <leticia.negreiros@outlook.com>
2023-06-24 22:31:03 -0300

    fix: correção na lógica do código

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 $(document).ready(function () {
+  const arrayDeItens = [];
+
   $("form").submit(function (e) {
     e.preventDefault();
 
@@ -7,10 +9,14 @@ $(document).ready(function () {
 
     novoItem.appendTo("ul"); // Colocando a nova linha criada acima (contendo o valor inserido no input) dentro da lista ul
 
+    arrayDeItens.push(task);
+
+    console.log(arrayDeItens);
+
     $("#task").val("");
   });
 
-  $("ul li").click(function () {
-    $("ul li").toggleClass("task-completed");
-  });
+  $("ul").on("click", "li", function () {
+    $(this).toggleClass("completed");
+  }); // Adicionando escutador no elemento pai -> evento, elemento filho e ação
 });
