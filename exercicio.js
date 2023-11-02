4d99c006f5c4c0f611b550319f1c4ca97b7c440a
lichtle <leticia.negreiros@outlook.com>
2023-11-02 19:54:45 -0300

    chore: tarefa finalizada

---
+++ exercicio.js
@@ -0,0 +1,8 @@
+function multiplicar(x, y) {
+    return x * y;
+}
+console.log(multiplicar(2, 4));
+function saudar(nome) {
+    return "Ol\u00E1 ".concat(nome, "!");
+}
+console.log(saudar("Lichtle"));
---
+++ exercicio.ts
@@ -0,0 +1,11 @@
+function multiplicar(x: number, y: number): number {
+  return x * y;
+}
+
+console.log(multiplicar(2, 4));
+
+function saudar(nome: string): string {
+  return `Olá ${nome}!`;
+}
+
+console.log(saudar("Lichtle"));
